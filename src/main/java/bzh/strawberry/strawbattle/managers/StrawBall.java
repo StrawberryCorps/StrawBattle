afd78fff96188ecb79cceb84218a1e52e2717b8a
Eclixal <alex.jousset1@gmail.com>
2021-01-24 00:40:13 +0100

    manage the fireball

---
+++ src/main/java/bzh/strawberry/strawbattle/managers/StrawBall.java
@@ -0,0 +1,29 @@
+package bzh.strawberry.strawbattle.managers;
+
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+
+/*
+ * This file StrawBall is part of a project StrawBattle.StrawBattle.
+ * It was created on 24/01/2021 00:39 by Eclixal.
+ * This file as the whole project shouldn't be modify by others without the express permission from StrawberryCorps author(s).
+ *  Also this comment shouldn't get remove from the file. (see Licence)
+ */
+public class StrawBall {
+
+    private Fireball fireball;
+    private Player player;
+
+    public StrawBall(Fireball fireball, Player player) {
+        this.fireball = fireball;
+        this.player = player;
+    }
+
+    public Fireball getFireball() {
+        return fireball;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
