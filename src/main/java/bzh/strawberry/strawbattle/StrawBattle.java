c7843def103262b4ebfbb185f493ae098ac8671e
Eclixal <alex.jousset1@gmail.com>
2021-01-23 17:53:05 +0100

    make struct

---
+++ src/main/java/bzh/strawberry/strawbattle/StrawBattle.java
@@ -0,0 +1,11 @@
+package bzh.strawberry.strawbattle;
+
+/*
+ * This file StrawBattle is part of a project StrawBattle.StrawBattle.
+ * It was created on 23/01/2021 17:48 by Eclixal.
+ * This file as the whole project shouldn't be modify by others without the express permission from StrawberryCorps author(s).
+ *  Also this comment shouldn't get remove from the file. (see Licence)
+ */
+public class StrawBattle {
+
+}
